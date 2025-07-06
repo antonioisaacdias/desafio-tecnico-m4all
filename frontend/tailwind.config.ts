7275337e709d4b05d72fe4ea04f3a7a3d88fd036
antonioisaacdias <antonioisaacvd@gmail.com>
2025-07-06 18:30:26 -0300

    feat: Arquivos gerais do front end, global.css, configurações do tailwind, componente de terceiro utilizado no front.

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,69 +1,108 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        brand: "#670099",
-        brandHover: "#8811c2",
-        brandSecondary: "#370053",
-        background: "#EFEFEF",
-        backgroundSecondary: "#F1F2F1",
-        backgroundTertiary: "#FFFFFF",
-        text: "#413D3D",
-        textPlaceholder: "#989898",
-        border: "#C9D5E1",
-        danger: "#C42F29",
-        success: "#6FAF38",
-      },
-
-      fontFamily: {
-        inter: ["Inter", "sans-serif"],
-      },
-      fontSize: {
-        xs: "0.75rem", // 12px
-        sm: "0.875rem", // 14px
-        base: "1rem", // 16px
-        xl: "1.25rem", // 20px
-        "2xl": "1.5rem", // 24px
-        "3xl": "1.875rem", // 30px
-        "4xl": "2.25rem", // 36px
-      },
-      fontWeight: {
-        regular: "400",
-        medium: "500",
-        bold: "700",
-        light: "300",
-      },
-      boxShadow: {
-        sm: "0 1px 2px rgba(0, 0, 0, 0.05)",
-        DEFAULT: "0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.06)",
-      },
-      borderRadius: {
-        DEFAULT: "0.25rem", // 4px
-        md: "0.375rem", // 6px
-        lg: "0.5rem", // 8px
-        xl: "0.75rem", // 12px
-      },
-      spacing: {
-        0: "0px",
-        1: "4px", // 0.25rem
-        2: "8px", // 0.5rem
-        3: "12px", // 0.75rem
-        4: "16px", // 1rem
-        5: "20px", // 1.25rem
-        6: "24px", // 1.5rem
-        8: "32px", // 2rem
-        10: "40px", // 2.5rem
-      },
-    },
+  	extend: {
+  		colors: {
+  			brand: '#670099',
+  			brandHover: '#8811c2',
+  			background: 'hsl(var(--background))',
+  			backgroundSecondary: '#F1F2F1',
+  			text: '#413D3D',
+  			border: 'hsl(var(--border))',
+  			danger: '#C42F29',
+  			success: '#6FAF38',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			inter: [
+  				'Inter',
+  				'sans-serif'
+  			]
+  		},
+  		fontSize: {
+  			xs: '0.75rem',
+  			sm: '0.875rem',
+  			base: '1rem',
+  			xl: '1.25rem',
+  			'2xl': '1.5rem',
+  			'3xl': '1.875rem',
+  			'4xl': '2.25rem'
+  		},
+  		fontWeight: {
+  			regular: '400',
+  			medium: '500',
+  			bold: '700',
+  			light: '300'
+  		},
+  		boxShadow: {
+  			sm: '0 1px 2px rgba(0, 0, 0, 0.05)',
+  			DEFAULT: '0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.06)'
+  		},
+  		borderRadius: {
+  			DEFAULT: '0.25rem',
+  			md: 'calc(var(--radius) - 2px)',
+  			lg: 'var(--radius)',
+  			xl: '0.75rem',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		spacing: {
+  			'0': '0px',
+  			'1': '4px',
+  			'2': '8px',
+  			'3': '12px',
+  			'4': '16px',
+  			'5': '20px',
+  			'6': '24px',
+  			'8': '32px',
+  			'10': '40px'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 
 export default config;
