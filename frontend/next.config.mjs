6a5366141cac8fc04aab8c7cd0298b4180469abc
antonioisaacdias <antonioisaacvd@gmail.com>
2025-07-06 18:29:18 -0300

    config: Configurações da integração do docker entre o front e o back, .envs .envs.example, dockerfiles, compose, configurações do next e favicon.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    // Para otimização de produção
+    outputFileTracingRoot: undefined,
+  },
+};
 
 export default nextConfig;
